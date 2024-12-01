cc821489fb6429912566d0272b3af1d8c111f1ee
Blaine <blainesolomon@users.noreply.github.com>
2024-12-01 11:44:03 -0800

    Initial Commit

---
+++ Advent of Code 24/Advent_of_Code_24App.swift
@@ -0,0 +1,17 @@
+//
+//  Advent_of_Code_24App.swift
+//  Advent of Code 24
+//
+//  Created by Blaine on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Advent_of_Code_24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Advent of Code 24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Advent of Code 24
+//
+//  Created by Blaine on 12/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
